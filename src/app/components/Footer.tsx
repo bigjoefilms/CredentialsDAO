18007a73f72be261ec87d7f1e441cae63ecd3e42
Bigjoe.js <98628310+bigjoefilms@users.noreply.github.com>
2024-08-22 23:44:00 +0100

    commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,18 +12,19 @@ const Footer = () => {
        
         Credentials<span className="bg-gradient-to-r from-cyan-500 to-blue-500 py-1 px-2 rounded-lg text-[#fff]" > DAO</span>
       </Link>
-          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
-                <ul className="text-[14px] font-[400] flex flex-col gap-4 text-[#494848] ">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
-                      {link}
-                    </Link>
-                  ))}
-                </ul>
-              </FooterColumn>
-            ))}
+      <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+  {FOOTER_LINKS.map((columns, index) => (
+    <FooterColumn title={columns.title} key={index}>
+      <ul className="text-[14px] font-[400] flex flex-col gap-4 text-[#494848] ">
+        {columns.links.map((link, linkIndex) => (
+          <Link href="/" key={linkIndex}>
+            {link}
+          </Link>
+        ))}
+      </ul>
+    </FooterColumn>
+  ))}
+ 
 
             <div className="flex flex-col gap-5 ">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
