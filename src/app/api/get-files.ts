6e5ecb566219b5022bb344e7d3a605babb2b83e3
Bigjoe.js <98628310+bigjoefilms@users.noreply.github.com>
2024-08-27 12:24:24 +0100

    added

--- src/app/api/get-files.ts
+++ src/app/api/get-files.ts
@@ -1,32 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import axios from 'axios';
 
-const pinataJWT = process.env.NEXT_PUBLIC_PINATA_JWT;
+// const pinataJWT = process.env.NEXT_PUBLIC_PINATA_JWT;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method !== 'GET') {
+//     return res.status(405).json({ message: 'Method not allowed' });
+//   }
 
-  try {
-    const response = await axios.get('https://api.pinata.cloud/data/pinList', {
-      headers: {
-        Authorization: `Bearer ${pinataJWT}`,
-      },
-      params: {
-        // Filter files based on user-specific metadata if available
-        // metadata[key]: value
-      },
-    });
+//   try {
+//     const response = await axios.get('https://api.pinata.cloud/data/pinList', {
+//       headers: {
+//         Authorization: `Bearer ${pinataJWT}`,
+//       },
+//       params: {
+//         // Filter files based on user-specific metadata if available
+//         // metadata[key]: value
+//       },
+//     });
 
-    const files = response.data.rows.map((file: any) => ({
-      name: file.metadata.name,
-      hash: file.ipfs_pin_hash,
-    }));
+//     const files = response.data.rows.map((file: any) => ({
+//       name: file.metadata.name,
+//       hash: file.ipfs_pin_hash,
+//     }));
 
-    res.status(200).json({ files });
-  } catch (error) {
-    console.error("Error fetching files:", error);
-    res.status(500).json({ message: "Error fetching files" });
-  }
-}
+//     res.status(200).json({ files });
+//   } catch (error) {
+//     console.error("Error fetching files:", error);
+//     res.status(500).json({ message: "Error fetching files" });
+//   }
+// }
--- src/app/dashboard/drive/page.tsx
+++ src/app/dashboard/drive/page.tsx
@@ -2,11 +2,43 @@
 import React from "react";
 import DashboardLayout from '../../components/DashboardLayout';
 import { useOCAuth} from '@opencampus/ocid-connect-js'
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import Image from "next/image";
 import { useRef } from 'react';
 import { toPng } from 'html-to-image';
 import { PinataSDK } from "pinata";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { Web3Auth } from "@web3auth/modal";
+
+import RPC from "./ethersRPC";
+// import RPC from "./viemRPC";
+// import RPC from "./web3RPC";
+
+const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0xaa36a7",
+  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  // Avoid using public rpcTarget in production.
+  // Use services like Infura, Quicknode etc
+  displayName: "Ethereum Sepolia Testnet",
+  blockExplorerUrl: "https://sepolia.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+  logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+};
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: { chainConfig },
+});
+
+const web3auth = new Web3Auth({
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+  privateKeyProvider,
+});
 
 
 // Initialize Pinata SDK with environment variables
@@ -29,6 +61,8 @@ const Drive: React.FC = () => {
     const [errors, setErrors] = useState<FormErrors>({});
     console.log("ocAuth",ocAuth)
     const certificateRef = useRef(null);
+    const [provider, setProvider] = useState<IProvider | null>(null);
+    const [loggedIn, setLoggedIn] = useState(false);
 
   const handleDownloadImage = () => {
     if (certificateRef.current === null) {
@@ -151,6 +185,131 @@ const Drive: React.FC = () => {
     // Add other fields as necessary
   };
  
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
+
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    const web3authProvider = await web3auth.connect();
+    setProvider(web3authProvider);
+    if (web3auth.connected) {
+      setLoggedIn(true);
+    }
+  };
+  const getUserInfo = async () => {
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const signedMessage = await RPC.signMessage(provider);
+    uiConsole(signedMessage);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    uiConsole("Sending Transaction...");
+    const transactionReceipt = await RPC.sendTransaction(provider);
+    uiConsole(transactionReceipt);
+  };
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+      console.log(...args);
+    }
+  }
+
+  
+  
+
+  
+  const logout = async () => {
+    await web3auth.logout();
+    setProvider(null);
+    setLoggedIn(false);
+    uiConsole("logged out");
+  };
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const balance = await RPC.getBalance(provider);
+    uiConsole(balance);
+  };
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const address = await RPC.getAccounts(provider);
+    uiConsole(address);
+  };
+
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+    </>
+  );
 
   return (
     <DashboardLayout>
@@ -177,6 +336,11 @@ const Drive: React.FC = () => {
         >
           Create New
         </button>
+
+        <div className="grid">{loggedIn ? loggedInView : unloggedInView}</div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
         
         </div>
         
